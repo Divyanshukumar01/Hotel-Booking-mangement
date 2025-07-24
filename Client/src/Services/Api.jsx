72fc060860374abca5f7cbe99f5411565a670fbf
Divyanshukumar01 <divyanshusambariya@gmail.com>
2025-07-24 17:31:24 +0530

    Update Api.jsx

--- Client/src/Services/Api.jsx
+++ Client/src/Services/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const api = axios.create({ baseURL: 'https://hotel-booking-mangement-12.onrender.com/api' });
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://hotel-booking-mangement-12.onrender.com/api' });
+// const api = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 export default api;
